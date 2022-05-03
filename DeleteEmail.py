d6c23b81666727b4e44d764b668e3e8552f7bc44
Blag <atejada@gmail.com>
2022-05-03 15:07:51 -0400

    Update DeleteEmail.py

--- DeleteEmail.py
+++ DeleteEmail.py
@@ -6,21 +6,22 @@ import os
 import sys
 
 nylas = APIClient(
-	os.environ.get('CLIENT_ID'),
-	os.environ.get('CLIENT_SECRET'),
-	os.environ.get('ACCESS_TOKEN')
+    os.environ.get('CLIENT_ID'),
+    os.environ.get('CLIENT_SECRET'),
+    os.environ.get('ACCESS_TOKEN')
 )
 
 messageId = "<MESSAGE_ID>"
+
 labelsDict = {}
 labels = nylas.labels.all()
 for label in labels:
-	labelsDict[label["name"]] = label["id"] 
+    labelsDict[label["name"]] = label["id"] 
 
 try:
-	message = nylas.messages.get(messageId)
-	message.add_label(labelsDict["trash"])
-	message.save()
-	print(f"Your message was succesfully deleted")
+    message = nylas.messages.get(messageId)
+    message.add_label(labelsDict["trash"])
+    message.save()
+    print(f"Your message was succesfully deleted")
 except:
-	print(f"An {sys.exc_info()[0]} error ocurred. \nMessage was not found.")
+    print(f"An {sys.exc_info()[0]} error ocurred. \nMessage was not found.")
